d1c599ba19264737d93a31739c0dd683d8e62e78
RociRo <rossmintka14@gmail.com>
2015-05-06 13:28:21 -0500

    Subiendo primera parte de proxy

---
+++ OtrosCodigos/ServidorWeb.java
@@ -0,0 +1,52 @@
+import java.net.*;
+import java.io.*;
+public class ServidorWeb {
+    public static void main(String args[])
+    {
+            ServerSocket s;
+            System.out.println("Servidor web iniciado en el puerto 90");
+            try {
+                // Crea el socket del servidor inicial
+                s = new ServerSocket(90);
+            } catch ( Exception e ) {
+            System.out.println("Error: " + e );
+            return;
+        }
+        System.out.println("Esperando Conexiones");
+        while ( true ) {
+            try {
+                // espera por una conexion
+                Socket remote = s.accept();
+                // se acepta la conexion
+                System.out.println("Conexion, enviando datos.");
+                BufferedReader in = new BufferedReader( new InputStreamReader(remote.getInputStream()) );
+                PrintWriter out = new PrintWriter(remote.getOutputStream());
+                // leer los datos enviados,
+                // para de leer hasta que lee el fin de linea, es decir la linea en blanco
+                // la linea en blaco es la señal de fin de las cabeceras HTTP
+                String linea=".";
+                //System.out.println(linea);		
+				//linea = in.readLine();
+   				// Manda la respuesta
+                // Manda las cabeceras
+				do {
+				linea = in.readLine();//Recupera lo que envío el cliente
+   				System.out.println("El usuario tecleo: " + linea);
+			    //out.println(linea);						            				
+				}while ( !linea.equals("") );
+                out.println("HTTP/1.0 200 OK");
+                out.println("Content-Type: text/html");
+                // esta línea en blanco indica el final de las cabeceras
+                out.println("");
+                // Mandamos la pagina HTML
+                out.println( "<H1>Hola</H2>");
+                out.println( "<p>Probando</p>");
+                out.flush(); //envía los datos
+				
+                remote.close();
+            } catch ( Exception e ) {
+                System.out.println("Error: " + e );
+            }
+        }
+    }
+}
