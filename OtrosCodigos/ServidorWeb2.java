4d352ea01948628f4cb5d2b211961e1c4b749dee
RociRo <rossmintka14@gmail.com>
2015-05-10 14:07:00 -0500

    Ya casi queda, falta formato desde el server

--- OtrosCodigos/ServidorWeb2.java
+++ OtrosCodigos/ServidorWeb2.java
@@ -24,34 +24,64 @@ public class ServidorWeb2 {
                 //PrintWriter sal_servidor1 = new PrintWriter(cli_servidor1.getOutputStream());
                 //Para enviar respuestas al cliente
    				DataOutputStream sal_servidor1 = new DataOutputStream(cli_servidor1.getOutputStream());
-   				String linea;
+   				String linea, cadenaPag, otra;
                 linea = ".";
-                do {
-                    linea = en_servidor1.readLine();//Recupera lo que envío el cliente
+                do {         
+                	linea = en_servidor1.readLine();
+                	otra = linea.substring(7, linea.length());
                     System.out.println("El servidor envía: " + linea);
                     System.out.println("El servidor envía............");
-                    System.out.println("uno dos");
+                    System.out.println("uno dos" + otra);
                     sal_servidor1.writeUTF("Hola mundo :) desde sockets.");
+                    try { 
+                        /*Socket socket= new Socket (otra, 80);//Me conecto a la página que haya enviado por el puerto 80
+                        //Para recuperar la respuesta del server
+                        DataInputStream	out2 = new DataInputStream(socket.getInputStream());
+                        System.out.println("Aquí entra...");
+                        do {
+                        	cadenaPag = out2.readUTF();//Recupera lo que envío el cliente
+                            System.out.println("Leer del recurso consultado: " + cadenaPag);
+                            sal_servidor1.writeUTF(cadenaPag);
+					            				
+                        }while ( !cadenaPag.equals(null) );*/
+                        /*Socket ser_internet= new Socket (otra, 80);// Se hace la conexión al servidor
+                        System.out.println("1");
+                        //para enviar el server privado
+                        PrintWriter sal_internet = new PrintWriter(ser_internet.getOutputStream(),true);
+                        //Para recuperar la respuesta del server
+                        System.out.println("2");
+                        BufferedReader en_internet = new BufferedReader( new InputStreamReader(ser_internet.getInputStream()) );
+                        System.out.println("3");
+                        //DataInputStream en_internet = new DataInputStream(ser_internet.getInputStream());
+                        //sal_internet.println(linea);//Escribo al server
+                        System.out.println("Leer del recurso consultado:::: " +en_internet.readLine());
+                        while (!(cadenaPag = en_internet.readLine()).equals(null)){ //imprimir respuesta del servidor
+                  		  System.out.println("Leer del recurso consultado: " +cadenaPag);
+                  		  sal_servidor1.writeUTF(cadenaPag);
+                  	  	}*/
+                    	URL url = new URL(linea);//Convertimos en url lo que nos mando el server público
+                	    URLConnection uc = url.openConnection();
+                	    uc.connect();
+                	    //Creamos el objeto con el que vamos a leer
+                	    BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+                	    String inputLine = ".";
+                	    String contenido = "";
+                	      
+                	    while ((inputLine = in.readLine()) != null) {
+                	        contenido += inputLine + "\n";
+                	    }
+                	    sal_servidor1.writeUTF(contenido);
+                	    in.close();
+                	    
+                        
+                    } catch ( Exception e ) {
+                            System.out.println("Error: " + e );
+                    }                    
+                    
                 }while (linea.length()!=0);    
-				//} while ((linea = en_servidor1.readLine()) != null);
-                sal_servidor1.writeUTF("Hola mundo :) desde sockets");
-                //    sal_servidor1.println("Hola mundo :) desde sockets");
                 if (linea.trim().equals(null))
                     return;
-                /*DataOutputStream salida = new DataOutputStream(cli_servidor1.getOutputStream());                
-                try {    
-                    Socket socket= new Socket (en_servidor1.readLine(),80);//Me conecto a la página que haya enviado por el puerto 80
-                    //Para recuperar la respuesta del server
-                    DataInputStream	out2 = new DataInputStream(socket.getInputStream());
-                    do {
-                        cadenaPagina = out2.readUTF();//Recupera lo que envío el cliente
-                        System.out.println("Leer: " + cadenaPagina);
-                        salida.writeUTF(cadenaPagina);
-			    //out.println(linea);						            				
-                    }while ( !cadenaPagina.equals("") );
-                } catch ( Exception e ) {
-                        System.out.println("Error: " + e );
-                }*/
+                /*DataOutputStream salida = new DataOutputStream(cli_servidor1.getOutputStream());      */
                 en_servidor1.close();// Se cierra la conexión remota
                 cli_servidor1.close();
                 sal_servidor1.close();// 
