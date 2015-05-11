c8570d51ebec885bb0514784075e9d003f9fcc14
RociRo <rossmintka14@gmail.com>
2015-05-11 16:37:52 -0500

    Correcciones

--- OtrosCodigos/ServidorWeb2.java
+++ OtrosCodigos/ServidorWeb2.java
@@ -17,8 +17,15 @@ public class ServidorWeb2 {
             try {
                 // espera por una conexion del servidor 1 para enviar y recibir
             	// se acepta la conexión
+            	Socket ser_publico= new Socket ("cags.com.mx", 5100);// Se hace la conexión al servidor
+                System.out.println(" Entro aquí 1");
                 Socket cli_servidor1 = s2.accept();
                 System.out.println("Conexión, enviando y recibiendo datos.");
+                System.out.println(" Entro aquí");
+                PrintWriter salida_servidor1 = new PrintWriter(ser_publico.getOutputStream(),true);
+                //Para recuperar la respuesta del server
+                DataInputStream entrada_servidor1 = new DataInputStream(ser_publico.getInputStream());
+                /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////7
                 BufferedReader en_servidor1 = new BufferedReader( new InputStreamReader(cli_servidor1.getInputStream()) );
                 //PrintWriter sal_servidor1 = new PrintWriter(cli_servidor1.getOutputStream());
                 //Para enviar respuestas al cliente
@@ -31,7 +38,7 @@ public class ServidorWeb2 {
                     System.out.println("El servidor envía: " + linea);
                     System.out.println("El servidor envía............");
                     System.out.println("uno dos" + otra);
-                    sal_servidor1.writeUTF("Hola mundo :) desde sockets.");
+                    //sal_servidor1.writeUTF("Hola mundo :) desde sockets.");
                     try { 
                     	URL url = new URL(linea);//Convertimos en url lo que nos mando el server público
                 	    URLConnection uc = url.openConnection();
